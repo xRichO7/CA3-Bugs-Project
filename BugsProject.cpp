197cec887908a758f7d82084dca5675eef050363
Richie <d00280247@student.dkit.ie>
2026-04-27 09:48:44 +0100

    Constructing repo

---
+++ BugsProject.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jerro on 27/04/2026.
+//
+
+#include "BugsProject.h"
---
+++ BugsProject.h
@@ -0,0 +1,14 @@
+//
+// Created by jerro on 27/04/2026.
+//
+
+#ifndef CA3_BUGS_PROJECT_BUGSPROJECT_H
+#define CA3_BUGS_PROJECT_BUGSPROJECT_H
+
+
+class BugsProject
+{
+};
+
+
+#endif //CA3_BUGS_PROJECT_BUGSPROJECT_H
