04bdff48517cbbc10b56c369ba716e5a4c97f916
danielvin1 <acebtg0@gmail.com>
2026-05-05 23:14:34 +0100

    tidied up code

--- Hopper.cpp
+++ Hopper.cpp
@@ -4,61 +4,31 @@
 Hopper::Hopper(int id, int x, int y, int directionValue, int health, int hopLen)
     : Bug(id, x, y, directionValue, health), hopLength(hopLen) {}
 
-void Hopper::move(int boardSize) {
+void Hopper::move(int boardSize) 
+{
     if (!alive) return;
 
-    if (isWayBlocked(boardSize)) {
-        do {
+   
+    if (isWayBlocked(boardSize)) 
+    {
+        do 
+        {
             direction = 1 + rand() % 4;
         } while (isWayBlocked(boardSize));
     }
 
-    int dx = 0;
-    int dy = 0;
-    if (direction == 1) dy = -hopLength;
-    else if (direction == 2) dx = hopLength;
-    else if (direction == 3) dy = hopLength;
-    else if (direction == 4) dx = -hopLength;
+    int dx = 0, dy = 0;
 
+    if (direction == 1) dy = -hopLength; //north
+    else if (direction == 2) dx = hopLength; //east
+    else if (direction == 3) dy = hopLength; //south
+    else if (direction == 4) dx = -hopLength; //west
     int newX = position.first + dx;
     int newY = position.second + dy;
 
     if (newX < 0) newX = 0;
     else if (newX >= boardSize) newX = boardSize - 1;
-    if (newY < 0) newY = 0;
-    else if (newY >= boardSize) newY = boardSize - 1;
 
-    setPosition(newX, newY);
-}
-
-
-#include "Hopper.h"
-#include <cstdlib>
-
-Hopper::Hopper(int id, int x, int y, int directionValue, int health, int hopLen)
-    : Bug(id, x, y, directionValue, health), hopLength(hopLen) {}
-
-void Hopper::move(int boardSize) {
-    if (!alive) return;
-
-    if (isWayBlocked(boardSize)) {
-        do {
-            direction = 1 + rand() % 4;
-        } while (isWayBlocked(boardSize));
-    }
-
-    int dx = 0;
-    int dy = 0;
-    if (direction == 1) dy = -hopLength;
-    else if (direction == 2) dx = hopLength;
-    else if (direction == 3) dy = hopLength;
-    else if (direction == 4) dx = -hopLength;
-
-    int newX = position.first + dx;
-    int newY = position.second + dy;
-
-    if (newX < 0) newX = 0;
-    else if (newX >= boardSize) newX = boardSize - 1;
     if (newY < 0) newY = 0;
     else if (newY >= boardSize) newY = boardSize - 1;
 
@@ -68,9 +38,11 @@ void Hopper::move(int boardSize) {
 bool Hopper::isWayBlocked(int boardSize) const {
     int x = position.first;
     int y = position.second;
-    if (direction == 1 && y < hopLength) return true;
-    if (direction == 2 && x >= boardSize - hopLength) return true;
-    if (direction == 3 && y >= boardSize - hopLength) return true;
-    if (direction == 4 && x < hopLength) return true;
+
+    if (direction == 1 && y < hopLength) return true; //north
+    if (direction == 2 && x >= boardSize - hopLength) return true;//east
+    if (direction == 3 && y >= boardSize - hopLength) return true;//south
+    if (direction == 4 && x < hopLength) return true; //west
+
     return false;
 }
