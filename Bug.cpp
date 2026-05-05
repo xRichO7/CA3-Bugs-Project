7d5383b201999620af500660bae4c0c30da60108
danielvin1 <acebtg0@gmail.com>
2026-05-05 22:46:59 +0100

    Initialise Bug constructor with position, direction, health and path tracking

--- Bug.cpp
+++ Bug.cpp
@@ -1,4 +1,15 @@
 #include "Bug.h"
 
-Bug::Bug() {
+Bug::Bug(int bugId, int positionX, int positionY, int directionValue, int bugHealth) {
+    id = bugId;
+    position.first = positionX;
+    position.second = positionY;
+    direction = directionValue;
+    health = bugHealth;
+    alive = true;
+    path.push_back(position);
+
 }
+
+
+
