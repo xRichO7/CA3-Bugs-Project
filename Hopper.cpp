4712026b01df434551f526f079260928c77a4f98
danielvin1 <acebtg0@gmail.com>
2026-05-05 23:04:31 +0100

    Add Hopper constructor with hop length initialization

--- Hopper.cpp
+++ Hopper.cpp
@@ -1,5 +1,11 @@
 #include "Hopper.h"
 
-Hopper::Hopper() {
-}
+Hopper::Hopper(int id, int x, int y, int directionValue, int health, int hopLen)
+    : Bug(id, x, y, directionValue, health), hopLength(hopLen) 
+    
+    
+    {
 
+
+
+    }
