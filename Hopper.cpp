6fddefbac33b7d3fb35b419376fc433dc18f47ef
danielvin1 <acebtg0@gmail.com>
2026-05-05 23:08:32 +0100

    Implement Hopper movement with hop based directional updates

--- Hopper.cpp
+++ Hopper.cpp
@@ -1,11 +1,34 @@
 #include "Hopper.h"
+#include <cstdlib>
 
 Hopper::Hopper(int id, int x, int y, int directionValue, int health, int hopLen)
-    : Bug(id, x, y, directionValue, health), hopLength(hopLen) 
-    
-    
-    {
+    : Bug(id, x, y, directionValue, health), hopLength(hopLen) {}
 
+void Hopper::move(int boardSize) {
+    if (!alive) return;
 
-
+    if (isWayBlocked(boardSize)) {
+        do {
+            direction = 1 + rand() % 4;
+        } while (isWayBlocked(boardSize));
     }
+
+    int dx = 0;
+    int dy = 0;
+    if (direction == 1) dy = -hopLength;
+    else if (direction == 2) dx = hopLength;
+    else if (direction == 3) dy = hopLength;
+    else if (direction == 4) dx = -hopLength;
+
+    int newX = position.first + dx;
+    int newY = position.second + dy;
+
+    if (newX < 0) newX = 0;
+    else if (newX >= boardSize) newX = boardSize - 1;
+    if (newY < 0) newY = 0;
+    else if (newY >= boardSize) newY = boardSize - 1;
+
+    setPosition(newX, newY);
+}
+
+
