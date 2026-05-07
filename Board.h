d3b51009493652bd5681986a3f5b777f17e51577
danielvin1 <acebtg0@gmail.com>
2026-05-07 09:42:29 +0100

    reorganize includes and format Board class

--- Board.h
+++ Board.h
@@ -1,10 +1,30 @@
 #ifndef BOARD_H
 #define BOARD_H
 
-class Board {
+#include <vector>
+#include <memory>
+#include <string>
+
+#include "Bug.h"
+
+class Board
+{
+private:
+    std::vector<std::unique_ptr<Bug>> bugs;
+    int boardSize;
+
 public:
     Board();
+
+    void loadBugs(const std::string& filename);
+
+    void displayAllBugs() const;
+
+    Bug* findBug(int id) const;
+
+    void tapBoard();
+
+    std::vector<std::unique_ptr<Bug>>& getBugs();
 };
 
 #endif
-
